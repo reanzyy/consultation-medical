8374ac21ee95bf815341e4a83fd408d4924d5c0d
Chandra Ardiansyah <chandraardiansyah265@gmail.com>
2023-11-10 10:02:59 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                         <div class="card-body">
                             <h5 class="card-title text-primary">Welcome back {{ auth()->user()->name }}!</h5>
                             <p class="mb-4">
-                                You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
+                                Arip jelek <span class="fw-bold">72%</span> more sales today. Check your new badge in
                                 your profile.
                             </p>
 
